6990dda6db130f95d0a478de0847dbbde023cb38
Michael Hollenberg <michael.hollenberg@uconn.edu>
2018-04-09 23:32:30 -0400

    first commits

---
+++ liri.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+// Here we would put the code to import the keys.js File. 
+
+
+// Here we would set the commands for my tweets, spotify and movie. 
+// We would do this by using process.argv[2] + what user types
